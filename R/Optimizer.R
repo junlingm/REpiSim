9980547663d34fef4e2a2fa5476803159c563e5e
Junling Ma <junlingm@uvic.ca>
2025-08-04 01:40:12 -0700

    Update documentation

--- R/Optimizer.R
+++ R/Optimizer.R
@@ -1,4 +1,7 @@
 #' A subclass of Calibrator that uses an optimizer and an initial guess.
+#' @name Optimizer
+#' @docType class
+#' @export
 Optimizer <- R6::R6Class(
   "Optimizer",
   inherit = Calibrator,
@@ -8,39 +11,36 @@ Optimizer <- R6::R6Class(
       ODE$new(model)
     },
     
-    #' The objective function for optimization
-    #' This method must be implemented by subclasses.
-    #' @param par the values of the fitted parameters
-    #' @param formula gives the parameter values that should be calculated
-    #' using this formula
-    #' @param fixed the parameter values that are given
-    #' @return the fitting results, which will be passed to 
-    #' the intepret method.
+    ## The objective function for optimization
+    ## @param par the values of the fitted parameters
+    ## @param formula gives the parameter values that should be calculated
+    ## using this formula
+    ## @param fixed the parameter values that are given
+    ## @return the fitting results, which will be passed to the intepret method.
+    ## @details This method must be implemented by subclasses.
     objective = function(par, formula, fixed, ...) {
       stop("The objective method must be implemented by a subclass")
     },
     
-    #' The optimization is done in this function.
-    #' This method must be implemented by subclasses.
-    #' @param guess the initial guesses of the fitted parameters
-    #' @param formula gives the parameter values that should be calculated
-    #' using this formula
-    #' @param fixed the parameter values that are given
-    #' @return the fitting results, which will be passed to 
-    #' the intepret method.
+    ## The optimization is done in this function.
+    ## This method must be implemented by subclasses.
+    ## @param guess the initial guesses of the fitted parameters
+    ## @param formula gives the parameter values that should be calculated using this formula
+    ## @param fixed the parameter values that are given
+    ## @return the fitting results, which will be passed to 
+    ## the intepret method.
     optimizer = function(guess, formula, fixed, ...) {
       stop("The optimizer method must be implemented by a subclass")
     },
     
-    #' The actual calibration is done in this function.
-    #' This method Must be implemented by subclasses.
-    #' @param fit the parameter values that should be fitted
-    #' @param formula gives the parameter values that should be calculated
-    #' using this formula
-    #' @param fixed the parameter values that are given
-    #' @param guess the initial guess of the parameters to be fitted
-    #' @return the fitting results, which will be passed to 
-    #' the intepret method.
+    ## The actual calibration is done in this function.
+    ## This method Must be implemented by subclasses.
+    ## @param fit the parameter values that should be fitted
+    ## @param formula gives the parameter values that should be calculated using this formula
+    ## @param fixed the parameter values that are given
+    ## @param guess the initial guess of the parameters to be fitted
+    ## @return the fitting results, which will be passed to 
+    ## the intepret method.
     .calibrate = function(fit, formula, fixed, guess, ...) {
       if (!is.numeric(guess) || any(is.na(guess)))
         stop("invalid initial values")
